13b84651669ce98122901a58430086ed47d790c4
urbezon <ubuntu@ip-172-31-29-175.us-west-2.compute.internal>
2013-07-20 14:17:51 +0000

    prueba en web.js

--- web.js
+++ web.js
@@ -4,13 +4,13 @@ var fs = require('fs');
 //var app = express.createServer(express.logger());
 var app = express();
 
-var content = fs.readFileSync("index.html");
+var content = fs.readFileSync("1stversion.html");
 
 app.get('/', function(request, response) {
   response.send(content.toString());
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
