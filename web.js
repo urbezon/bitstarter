5fccf402df5aa941356a98b1ae106f3b4fb926a3
urbezon <ubuntu@ip-172-31-29-175.us-west-2.compute.internal>
2013-08-07 18:38:26 +0000

    pruebas en web.js

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var fs = require('fs');
 
 //var app = express.createServer(express.logger());
@@ -9,6 +9,20 @@ var content = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
   //response.send(content.toString());
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+*/
+
+var express = require("express");
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
